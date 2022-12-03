d8f85a3dd5aca15b7f4e5c41fe4e1f8ea7007b1d
MomollyWang <wang.xinwa@northeastern.edu>
2022-12-03 11:26:35 -0800

    tabs at mainactivity set up

--- app/src/main/java/com/example/young_forest/MainActivity.kt
+++ app/src/main/java/com/example/young_forest/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.young_forest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableRow
+import androidx.viewpager.widget.ViewPager
+import com.example.young_forest.Adapter.MyAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.TabView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabView: TabLayout
+    private lateinit var viewPager: ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tabView = findViewById(R.id.tabView)
+        viewPager = findViewById(R.id.viewPager)
+
+        tabView.addTab(tabView.newTab().setText("Eng To Mor"))
+        tabView.addTab(tabView.newTab().setText("Mor To Eng"))
+        tabView.tabGravity = TabLayout.GRAVITY_FILL
+
+        val adapter = MyAdapter(this, supportFragmentManager, tabView.tabCount)
+        viewPager.adapter = adapter
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabView))
+        tabView.addOnTabSelectedListener(object :TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                viewPager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 }
