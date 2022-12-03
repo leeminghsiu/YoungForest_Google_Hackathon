6881a1a67119f538e06f94a8d4f964e854d12cfd
MomollyWang <wang.xinwa@northeastern.edu>
2022-12-03 15:58:54 -0800

    MorToEng input & clear buttons and textview set up

--- app/src/main/java/com/example/young_forest/Fragments/MorToEng.kt
+++ app/src/main/java/com/example/young_forest/Fragments/MorToEng.kt
@@ -5,56 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.young_forest.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MorToEng.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MorToEng : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mor_to_eng, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_mor_to_eng, container, false)
+        val input = view.findViewById<TextView>(R.id.morseText)
+        val output = view.findViewById<TextView>(R.id.morseTrans)
+        var morseText = ""
+        var morseTrans = ""
+
+        view.findViewById<Button>(R.id.morseDot).setOnClickListener {
+            morseText += "."
+            input.setText(morseText)
+        }
+
+        view.findViewById<Button>(R.id.morseLine).setOnClickListener {
+            morseText += "-"
+            input.setText(morseText)
+        }
+
+        view.findViewById<Button>(R.id.morseBlank).setOnClickListener {
+            morseText += " "
+            input.setText(morseText)
+        }
+
+        view.findViewById<Button>(R.id.clear1).setOnClickListener {
+            morseText = ""
+            input.setText("This is your input")
+        }
+
+        /***
+        view.findViewById<Button>(R.id.translate1).setOnClickListener{
+            morseTrans = morTransToEng(morseText)
+            output.setText(morseTrans)
+        }
+        **/
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MorToEng.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MorToEng().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
