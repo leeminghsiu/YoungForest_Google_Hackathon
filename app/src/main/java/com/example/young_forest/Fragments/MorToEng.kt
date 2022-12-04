30940b873f4c069453034eb6650eca7c17fa9cf7
MomollyWang <wang.xinwa@northeastern.edu>
2022-12-03 16:44:14 -0800

    MorToEng translation hashmap completed(including letters and digits)

--- app/src/main/java/com/example/young_forest/Fragments/MorToEng.kt
+++ app/src/main/java/com/example/young_forest/Fragments/MorToEng.kt
@@ -12,11 +12,21 @@ import com.example.young_forest.R
 
 class MorToEng : Fragment() {
 
+    var morToEngHashMap: HashMap<String, String> = hashMapOf(
+        ".-" to "a", "-..." to "b", "-.-." to "c", "-.." to "d",
+        "." to "e", "..-." to "f", "--." to "g", "...." to "h",
+        ".." to "i", ".---" to "j", "-.-" to "k", ".-.." to "l",
+        "--" to "m", "-." to "n", "---" to "o", ".--." to "p",
+        "--.-" to "q", ".-." to "r", "..." to "s", "-" to "t",
+        "..-" to "u", "...-" to "v", ".--" to "w", "-..-" to "x",
+        "-.--" to "y", "--.." to "z", ".----" to "1", "..---" to "2",
+        "...--" to "3", "....-" to "4", "....." to "5", "-...." to "6",
+        "--..." to "7", "---.." to "8", "----." to "9", "-----" to "0"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,17 +57,35 @@ class MorToEng : Fragment() {
 
         view.findViewById<Button>(R.id.clear1).setOnClickListener {
             morseText = ""
+            morseTrans = ""
             input.setText("This is your input")
+            output.setText("This is the translation")
         }
 
-        /***
         view.findViewById<Button>(R.id.translate1).setOnClickListener{
             morseTrans = morTransToEng(morseText)
             output.setText(morseTrans)
         }
-        **/
 
         return view
+    }
+
+    /**
+     * This fun translate Morse Code to English
+     */
+    private fun morTransToEng(morse: String): String {
+        var delimiter = " "
+        var transText = ""
+        val morseCode = morse.split(delimiter)
+        for (code in morseCode){
+            if (code != "" || code!= " " || code!= null){
+                var toEng = morToEngHashMap.get(code)
+                if ( toEng != null){
+                    transText += toEng
+                }
+            }
+        }
+        return transText
     }
 
 }
